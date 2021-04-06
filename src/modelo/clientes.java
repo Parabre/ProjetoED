bff27811dc58b8ffdb7174892a51af7cfb444411
Daniel Fortes <fortesgdaniel@gmail.com>
2021-04-06 19:49:35 -0300

    Adicionado classe cliente!

---
+++ src/modelo/clientes.java
@@ -0,0 +1,59 @@
+package modelo;
+
+public class clientes {
+	
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+	private String rg;
+	private String nome;
+	private int idade;
+	private String cep;
+	private String cpf;
+	private String email;
+	private String telefone;
+	
+
+}
